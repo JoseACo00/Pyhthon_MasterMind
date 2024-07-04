86248f17210d8834089e431da3c9e419b1fe7034
Josele Coca <josealx0211@gmail.com>
2024-07-04 14:53:45 +0200

    Mejorar el snake presentación

--- DATOS_CONTROL/Proyectos/Maze.py
+++ DATOS_CONTROL/Proyectos/Maze.py
@@ -14,7 +14,7 @@ NUM_MAX_OBJECTS_MAPS = 11
 tail_Lenght = 0
 tail = []
 my_position = [3,1]
-
+end_Game = False #VARIABLE PARA TERMINAR EL JUEGO / EN SWICH SERA FALSE HASTA QUE ESTE UN TRUE QUE SERA FIN DE JEUGO
 #HACER QUE SE GENEERE 10 DE ESTOS A LEATORIAMENTE
 
 map_Objects= []
@@ -23,12 +23,6 @@ map_Objects= []
 #     position_randomY = randint(2, 14)
 #     map_Objects.append([position_randomX, position_randomY ])
 
-while len(map_Objects) < NUM_MAX_OBJECTS_MAPS:
-
-    new_Position = [randint(0,MAP_WIDTH), randint(0,MAP_HEIGHT)]
-
-    if new_Position not in map_Objects and new_Position != my_position: #ES DISTINTO DE MI LISTA Y DIFERENTE EN MI POSICION INICIAL
-        map_Objects.append(new_Position)
 #DIBUJAR EL MAPA
 # print("+" + "-" * MAP_WIDTH * 3 + "+")
 #
@@ -72,10 +66,42 @@ while len(map_Objects) < NUM_MAX_OBJECTS_MAPS:
 #     print("|")
 # print("+" + "-" * MAP_WIDTH * 3 + "+")
 
+while len(map_Objects) < NUM_MAX_OBJECTS_MAPS:
 
+    new_Position = [randint(0,MAP_WIDTH), randint(0,MAP_HEIGHT)]
+
+    if new_Position not in map_Objects and new_Position != my_position: #ES DISTINTO DE MI LISTA Y DIFERENTE EN MI POSICION INICIAL
+        map_Objects.append(new_Position)
 #PARA HACER QUE NO SE REPITA TANTO LAS COSAS VAMOS A REALIZAR UN WHILE TRUE PARA QUE SEIMPRE REPITA LAS COSAS
 
-while True:
+# while True:
+#     # DIBUJAR EL MAPA
+#     print("+" + "-" * MAP_WIDTH * 3 + "+")
+#
+#     for cordinate_Y in range(MAP_HEIGHT):
+#         print("|", end="")
+#         for coordinate_x in range(MAP_WIDTH):
+#
+#             char_to_draw =" "
+#             tell_in_cell_nos = None
+#             #DIBUJAR LOS OBJETOS PARA RECOGER
+#             for map_Object in map_Objects:
+#                 if map_Object[POS_X] == coordinate_x and map_Object[POS_Y] == cordinate_Y:
+#                     char_to_draw = "º"
+#             #FOR DRAWING THE TAIL OF SNAKE
+#             for tail_piece in tail:
+#                 if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == cordinate_Y:
+#                     char_to_draw = "@"
+#             #DRAWING THE POSITION OF USER/SNAKE
+#             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == cordinate_Y:
+#                 char_to_draw = "@"
+#                 for map_Object in map_Objects:
+#                     if my_position[POS_X] == map_Object[POS_X] and my_position[POS_Y] == map_Object[POS_Y]:
+#                         map_Objects.remove(map_Object)
+#                         tail_Lenght += 1 #CRECE 1 MI COLA CUANDO COMEMEMOS UN CASILLA
+
+# MANERA DISTINTA CON LA VARIABLE OBJECT IN CELL
+while not end_Game:
     # DIBUJAR EL MAPA
     print("+" + "-" * MAP_WIDTH * 3 + "+")
 
@@ -83,22 +109,33 @@ while True:
         print("|", end="")
         for coordinate_x in range(MAP_WIDTH):
 
-            char_to_draw =" "
+            char_to_draw = " "
+            object_in_cell = None
+            tell_in_cell_ = None
+            # DIBUJAR LOS OBJETOS PARA RECOGER
 
-            #DIBUJAR LOS OBJETOS PARA RECOGER
             for map_Object in map_Objects:
                 if map_Object[POS_X] == coordinate_x and map_Object[POS_Y] == cordinate_Y:
                     char_to_draw = "º"
+                    object_in_cell = map_Object
 
+            # FOR DRAWING THE TAIL OF SNAKE
             for tail_piece in tail:
                 if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == cordinate_Y:
                     char_to_draw = "@"
+                    tell_in_cell_ = tail_piece
+
+            # DRAWING THE POSITION OF USER/SNAKE
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == cordinate_Y:
                 char_to_draw = "@"
-                for map_Object in map_Objects:
-                    if my_position[POS_X] == map_Object[POS_X] and my_position[POS_Y] == map_Object[POS_Y]:
-                        map_Objects.remove(map_Object)
-                        tail_Lenght += 1 #CRECE 1 MI COLA CUANDO COMEMEMOS UN CASILLA
+
+                if object_in_cell:
+                    map_Objects.remove(object_in_cell)
+                    tail_Lenght += 1  # CRECE 1 MI COLA CUANDO COMEMEMOS UN CASILLA
+
+                if tell_in_cell_:
+                    print("HAS MUERTO")
+                    end_Game = True
 
             print(" {} ".format(char_to_draw), end="")
         print("|")
@@ -140,5 +177,5 @@ while True:
         if my_position[POS_X] >= MAP_WIDTH:
             my_position[POS_X]= 0
     elif direction == "t":
-        break  #SALIMOS DEL BUCLE WHILE  ROMPE EL WHILE
+        end_Game = True
     os.system("cls")
