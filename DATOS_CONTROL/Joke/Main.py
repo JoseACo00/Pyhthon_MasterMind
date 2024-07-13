34d02fbdfbdac841fc77384c02bff162935d0cce
Josele Coca <josealx0211@gmail.com>
2024-07-13 13:23:25 +0200

    como conectarse a sqlite3

--- DATOS_CONTROL/Joke/Main.py
+++ DATOS_CONTROL/Joke/Main.py
@@ -1,21 +1,33 @@
 import os
+from time import sleep
+from random import randrange
+import sqlite3
+
 
 def main():
-    #Ruta del archivo donde se creará
-    desktop_path = "C:\\Users\\alexc\\Downloads\\"
 
+    #HACER QUE SE CREE EL ARCHIVO CUANDO PASE UNAS HORAS
+
+    x_hours = randrange(1,4)
+    sleep(x_hours * 60 * 60)
+
+    #Ruta del archivo donde se creará
+    # desktop_path = "C:\\Users\\alexc\\Downloads\\"
+
+    user_path = "C:\\Users\\alexc"
+    desktop_path = user_path + "\\Donwloads\\"
 
     # Crear archivo
-    a = open(desktop_path + "Para Ti.txt", "w")
+    # a = open(desktop_path + "Para Ti.txt", "w")
+    # #Escribir en el archivo
+    # a.write("SOY TU MAYOR MIEDO PERRO DE MIERDA")
+    # a.close()
 
-    #Escribir en el archivo
-    a.write("SOY TU MAYOR MIEDO PERRO DE MIERDA")
-    a.close()
+    #HACERLO DE UNA MANERA MÁS CHULA
+    with open( desktop_path + "XXX___s.txt", "w") as hackerFile:
+        hackerFile.write("H9LA SOY EL HACKER DE TIIII")
 
     #Obtener nombre del user
     # print(os.getlogin())
-
-
-
 if __name__ == '__main__':
     main()
